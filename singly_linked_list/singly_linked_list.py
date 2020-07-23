ae246ca79976972824be943837c1104e5e610a9d
mikepadiernos <mike.padiernos@gmail.com>
2020-07-22 23:36:03 -0400

    [FEATURE] module / singly_linked_list
    - partially worked on code

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,24 @@
+
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_tail(self, value):
+        new_node = Node(value, None)
