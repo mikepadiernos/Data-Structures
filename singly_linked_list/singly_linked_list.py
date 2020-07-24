2476e47d650dd27ff9dbea532d9bf2e164382ddf
mikepadiernos <mike.padiernos@gmail.com>
2020-07-23 22:47:48 -0400

    [FEATURE] module / linked list
    - finished code for singly_linked_list.py

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -20,12 +20,59 @@ class LinkedList:
         self.head = None
         self.tail = None
 
+    def add_to_head(self, value):
+        new_node = Node(value)
+
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.set_next(self.head)
+            self.head = new_node
+
     def add_to_tail(self, value):
-        new_node = Node(value, None)
-        if not self.head:
+        new_node = Node(value)
+
+        if not self.head and not self.tail:
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.set_next(new_node)
             self.tail = new_node
 
+    def remove_head(self):
+        if not self.head:
+            return None
+
+        if not self.head.get_next():
+            head = self.head
+            self.head = None
+            self.tail = None
+            return head.get_value()
+        value = self.head.get_value()
+        self.head = self.head.get_next()
+        return value
+
+    def contains(self, value):
+        if not self.head:
+            return False
+
+        current = self.head
+        while current:
+            if current.get_value() == value:
+                return True
+            current = current.get_next()
+
+        return False
+
+    def get_max(self):
+        if not self.head:
+            return None
+        else:
+            current = self.head
+            max_value = self.head.value
+            while current:
+                if current.value > max_value:
+                    max_value = current.value
+                current = current.get_next()
+            return max_value
